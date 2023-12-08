8a388a9ececbe500ed3cc8f37cfdc92fa08bf575
mToan <mtoanp@outlook.com>
2023-12-08 16:40:08 +0100

    v1

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
+
+// import { Fighter } from "./src/Fighter.js";
+const {Fighter} = require('./src/Fighter.js')
+
+const Heracles = new Fighter('🧔 Heracle', 20, 6)
+const Nemean = new Fighter('🦁 Namean Lion', 11, 13)
+
+// console.log(Heracles.fight(Nemean))
+// console.log(Nemean.life, ' -hp')
+
+let round = 0
+while (Heracles.life !== 0 && Nemean.life !==0) {
+    console.log(`--------------------------------------------`)
+    console.log(`Round ${round++}`)
+    Heracles.fight(Nemean)
+    Nemean.fight(Heracles)
+}
+
+console.log(`--------------------------------------------`)
+console.log(`${Heracles.isAlive ? Heracles.name : Nemean.name} est le survivant !`)
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,27 @@
+const MAX_LIFE = 100
+
 /* Fighter class definition */
+class Fighter {
+    constructor(name, strength, dexterity) {
+        this.name = name
+        this.strength = strength
+        this.dexterity = dexterity
+        this.life = MAX_LIFE
+    }
+
+    fight(fighter) {
+        const attack = Math.round(Math.random() * this.strength + 1);
+        const degat = (attack - fighter.dexterity > 0) ? attack - fighter.dexterity : 0
+        fighter.life = (fighter.life - degat > 0) ? fighter.life - degat : 0
+        console.log(`${this.name} 🗡️ (${attack}) ${fighter.name} , cause ${degat} degats | ${fighter.name} HP: ${fighter.life}`)
+    }
+
+    isAlive() {
+        return this.life > 0 ? true : false
+    }
+}
+
+// Exporting functions or variables
+module.exports = {
+    Fighter
+};
